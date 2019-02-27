1a85ee7eba217524b843a4ac8f1ddec19d7b3d65
srbberlin <info@hans-schrieber.de>
2019-02-27 14:58:59 +0100

    New version

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,68 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync')
-
+const del = require('del')
 
 var config = {
-  base:   __dirname + '/',
-  html:   __dirname + '/**/*.html',
-  css:    __dirname + '/css/**/*.css',
-  js:     __dirname + '/js/**/*.js',
-  img:    __dirname + '/img/**/*'
+  cssin:    __dirname + '/src/css/**/*.css',
+  jsin:     __dirname + '/src/js/**/*.js',
+  htmlin:   __dirname + '/src/html/**/*.html',
+  assetsin: __dirname + '/src/assets/**/*',
+  cssout:   __dirname + '/docs/css/',
+  jsout:    __dirname + '/docs/js/',
+  htmlout:  __dirname + '/docs/'
 }
 
-function reload () {
-  console.log('reload');
+function clean () {
+  return del([config.htmlout + '*'])
+}
+
+function reload (cb) {
   browserSync.reload()
+  cb()
 }
 
-function serve (cp) {
-  console.log('serve')
+function html () {
+  return gulp
+    .src(config.htmlin)
+    .pipe(gulp.dest(config.htmlout))
+}
+
+function assets () {
+  return gulp
+    .src(config.assetsin)
+    .pipe(gulp.dest(config.htmlout))
+}
+
+function css () {
+  return gulp
+    .src(config.cssin)
+    .pipe(gulp.dest(config.cssout))
+}
+
+function babl () {
+  return gulp
+    .src(config.jsin)
+    //.pipe(babel().on('error', e => {
+    //  console.log('babel', e)
+    //}))
+    .pipe(gulp.dest(config.jsout))
+}
+
+function serve () {
   browserSync({
-    server: config.base
+    server: config.htmlout
   })
 
-  gulp.watch(config.html, reload)
-  gulp.watch(config.css, reload)
-  gulp.watch(config.js, reload)
-  gulp.watch(config.img, reload)
-
-  cp();
+  gulp.watch(config.jsin, gulp.series(babl, reload))
+  gulp.watch(config.cssin, gulp.series(css, reload))
+  gulp.watch(config.htmlin, gulp.series(html, reload))
+  gulp.watch(config.assetsin, gulp.series(assets, reload))
 }
 
-exports.default = serve
+function build (cb) {
+  gulp.series(clean, gulp.parallel(html, assets, babl, css))(cb)
+}
+
+exports.build = build
+exports.clean = clean
+exports.default = gulp.series(build, serve)
